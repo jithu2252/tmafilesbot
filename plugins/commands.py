d9c68777da9eb5c64c9efbf42ea0727d75eedaeb
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2022-12-06 23:04:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,7 +50,7 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton('🔍 Search your movies here', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('💲 Get Premium Access', url='https://rigipay.com/g/ocU5Sv5Zgo')
+            InlineKeyboardButton('💲 Get Premium Access', url='https://Tmaadda.rpy.club/g/ocU5Sv5Zgo')
             
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -70,7 +70,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "💲 Get Access", url='https://rigipay.com/g/ocU5Sv5Zgo' #invite_link.invite_link
+                    "💲 Get Access", url='https://Tmaadda.rpy.club/g/ocU5Sv5Zgo' #invite_link.invite_link
                 )
             ]
         ]
@@ -99,7 +99,7 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton('🔍 Search Movies Here', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('💲 Get Premium Access', url='https://rigipay.com/g/ocU5Sv5Zgo')
+            InlineKeyboardButton('💲 Get Premium Access', url='https://Tmaadda.rpy.club/g/ocU5Sv5Zgo')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
